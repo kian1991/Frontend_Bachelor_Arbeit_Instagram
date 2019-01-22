46b34eaa95a1f2f3322740078f5dac4681ab9b9c
Kian Lütke <kianluetke@gmail.com>
2019-01-22 11:40:18 +0100

    Added Barchart for Hashtags and LineChart for ER

---
+++ js/instaCharts.js
@@ -0,0 +1,145 @@
+/**
+	Diese Klasse stellt die Funktionen zum darstellen der Diagramme bereit
+**/
+
+//Konstanten
+const INSTAGRAM_POST_URL = 'https://www.instagram.com/p/';
+
+/*
+	Diese Funktion ist für die Darstellung des Abonnenten Zugang/Abgang-Diagram
+ */
+function drawFollowerChart(follower_history) {
+	let data = new google.visualization.DataTable();
+	// Hier wird das Array mit den objekten in ein Array für das Diagramm gemapped
+	// und das Datum in ein Date Objekt gespeichert
+	const rows = follower_history.map(entry => {
+		let arr = Object.values(entry).reverse();
+		arr[0] = new Date(arr[0]*1000);
+		return arr;
+	});
+
+	// Daten hinzufügen
+	data.addColumn('date', 'Zeit');
+	data.addColumn('number', 'Abonnenten');
+	data.addRows(rows);
+	// Optionen festlegen
+	let options = {
+		title: 'Abonnenten Zugänge/Abgänge',
+		hAxis: {
+			title: 'Zeit'
+		},
+		vAxis: {
+			title: 'Abonnenten'
+		},explorer: {
+			actions: ['dragToZoom', 'rightClickToReset'],
+			axis: 'horizontal',
+			keepInBounds: true,
+			maxZoomIn: 6.0}
+	};
+	const chart = new google.visualization.LineChart(document.getElementById('follower-chart'));
+	chart.draw(data, options);
+}
+
+function drawMediaTimeEngagementChart(media) {
+	let data = new google.visualization.DataTable();
+	let rows = []
+	// Daten vorbereiten
+	for(let m of media){
+		let media_url = '';
+		const datetime = new Date(m.taken_at*1000);
+		if(m.image_versions2 === undefined){
+			media_url = m.carousel_media[0].image_versions2.candidates[1].url;
+		}else{
+			media_url = m.image_versions2.candidates[1].url
+		}
+		rows.push([
+			datetime,
+			m.engagement_rate,
+			'<img src="' +	media_url + '"><br><div style="margin: 0.25em; text-align: center; font-weight: bolder"> ' +
+			'Zeit: ' + datetime.format("HH:MM") + '</div>',
+			INSTAGRAM_POST_URL + m.code
+		])
+	}
+
+	// Daten hinzufügen
+	data.addColumn('date', 'Zeit');
+	data.addColumn('number', 'Engagement Rate');
+	data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+	data.addColumn({'type': 'string', 'role': 'link'});
+	data.addRows(rows);
+
+	// Optionen festlegen
+	let options = {
+		title: 'Engagementrate der Beiträge',
+		tooltip: { isHtml: true },
+		hAxis: {
+			title: 'Zeit'
+		},
+		vAxis: {
+			title: 'Engagement Rate'
+		},explorer: {
+			actions: ['dragToZoom', 'rightClickToReset'],
+			axis: 'horizontal',
+			keepInBounds: true,
+			maxZoomIn: 6.0}
+	};
+	const chart = new google.visualization.LineChart(document.getElementById('time-post-chart'));
+
+	// Links zur weiterleitung an die Instagram Seite
+	google.visualization.events.addListener(chart, 'select', function (e) {
+		const selection = chart.getSelection();
+		if (selection.length) {
+			const row = selection[0].row;
+			const url = data.getValue(row, 3);
+			const tab = window.open(url, '_blank');
+			tab.focus();
+		}
+	});
+	chart.draw(data, options);
+}
+
+function drawTopTenHashtags(hashtags) {
+
+	let data = new google.visualization.DataTable();
+	let rows = [];
+	//Daten Vorbereiten
+	for(let tag in hashtags){
+		rows.push([
+			tag,
+			hashtags[tag]
+		])
+	}
+
+	// Hashtags sortieren
+	rows = rows.sort(compareHashtag).splice(0,10);
+
+
+	// Daten hinzufügen
+	data.addColumn('string', '');
+	data.addColumn('number', 'Anzahl');
+	data.addRows(rows);
+	// Optionen festlegen
+	let options = {
+		title: 'Top 10 Hashtags',
+		hAxis: {
+			title: 'Anzahl'
+		},
+		vAxis: {
+			title: 'Hashtags'
+		},explorer: {
+			actions: ['dragToZoom', 'rightClickToReset'],
+			axis: 'horizontal',
+			keepInBounds: true,
+			maxZoomIn: 6.0}
+	};
+	const chart = new google.visualization.BarChart(document.getElementById('hashtag-chart'));
+	chart.draw(data, options);
+}
+
+
+
+// Helper functions
+
+function compareHashtag(hashtagA, hashtagB) {
+	return hashtagB[1] - hashtagA[1];
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*
 	Dieses Script ist.....
  */
+
 // Konstanten
 const API_URL = 'http://api.instalyzer.ml:5000/users/'
 const REFRESH_INTERVAL = 10 * 1000; // in ms
@@ -8,6 +9,7 @@ const API_URL_DEBUG = 'http://localhost:5000/users/'
 
 
 // Funktionen
+
 // Read a page's GET URL variables and return them as an associative array.
 function getFirstUrlParameter() {
     const query = window.location.href.slice(window.location.href.indexOf('?') + 1);
@@ -15,7 +17,6 @@ function getFirstUrlParameter() {
 }
 
 function getUserFromAPI(username, limit) {
-	let json = {};
 	$.ajax({
 		url: API_URL + username,
 		type: "GET",
@@ -45,6 +46,10 @@ function getUserFromAPI(username, limit) {
 	});
 }
 
+
+/*
+	Oberflächen verändernde funktionen
+*/
 function setLoading(isLoading) {
 	$('#loading-container').css('display', isLoading ? 'flex' : 'none');
 }
@@ -64,59 +69,32 @@ function fillBasicInfo(accountData){
 	$('#media').text(accountData.media_count)
 	if (accountData.is_private){
 		setPrivate();
-	}else if(accountData.follower_count_history) {
-		drawCharts(accountData.follower_count_history)
+	}else  {
+		drawCharts(accountData)
 	}
 }
 
 /**
-
- Visualisierung der Daten in google Charts
-
+ 	Visualisierung der Daten in google Charts
  **/
 
-function drawCharts(follower_history) {
+function drawCharts(accountData) {
 	google.charts.load('current', {packages: ['corechart', 'line']});
-	google.charts.setOnLoadCallback(() => drawBasic(follower_history));
+	if(accountData.follower_count_history.length > 1) {
+		google.charts.setOnLoadCallback(() => drawFollowerChart(accountData.follower_count_history));
+	}
+	if(accountData.media_count > 0) {
+		google.charts.setOnLoadCallback(() => drawMediaTimeEngagementChart(accountData.media));
+		google.charts.setOnLoadCallback(() => drawTopTenHashtags(accountData.hashtags));
+	}
 }
 
-
-function drawBasic(follower_history) {
-	const values = follower_history.map(entry => {
-		let arr = Object.values(entry).reverse();
-		arr[0] = new Date(arr[0]*1000);
-		return arr;
-	});
-	console.log(values)
-	let data = new google.visualization.DataTable();
-	data.addColumn('date', 'Zeit');
-	data.addColumn('number', 'Abonnenten');
-
-	data.addRows(values);
-
-	let options = {
-		hAxis: {
-			title: 'Time'
-		},
-		vAxis: {
-			title: 'Abonnenten'
-		},explorer: {
-			actions: ['dragToZoom', 'rightClickToReset'],
-			axis: 'horizontal',
-			keepInBounds: true,
-			maxZoomIn: 4.0}
-	};
-
-	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-	chart.draw(data, options);
-}
 
 
 // DEBUG
 
 $(document).ready(function () {
-	getUserFromAPI(getFirstUrlParameter(), 20)
+	getUserFromAPI(getFirstUrlParameter(), 200)
 });
 
 
